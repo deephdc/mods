084c11dcc316a05846c0a78acd54fa208d4ea62c
stevo <stefan.dlugolinsky@savba.sk>
2018-09-19 17:13:07 +0200

    MODS code import

--- mods/models/model.py
+++ mods/models/model.py
@@ -6,6 +6,53 @@ import pkg_resources
 # import project config.py
 import mods.config as cfg
 
+import os
+import sys
+import json
+import numpy as np
+import keras
+
+# column 1 - answer
+sample_data = np.array([
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0],
+    [0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1],
+    [0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1],
+    [0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1],
+    [0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1],
+    [0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0],
+])
+
+def load_model(model_file):
+    try:
+        model = keras.models.load_model(model_file)
+        model.predict(np.zeros((1, len(sample_data[0]) - 1)))
+        return model
+    except:
+        return None
+
+
+model_name = '2015.h5.python36'
+model_file = os.path.join(cfg.BASE_DIR, '..', 'models', model_name)
+
+model = load_model(model_file)
+if not model:
+    print('Could not load model: %s' % model_file)
+    sys.exit(1)
+else:
+    print('Model loaded: %s' % model_name)
+
+
+def get_sample_data():
+    global sample_data
+    return [json.dumps({
+        'labels': sample_data[:, 0].tolist(),
+        'rows': sample_data[:, 1:].tolist()
+    })]
+
 def get_metadata():
 
     module = __name__.split('.', 1)
@@ -41,7 +88,28 @@ def predict_data(*args):
     """
     Function to make prediction on an uploaded file
     """
-    message = 'Not implemented in the model (predict_data)'
+    if args and len(args) > 0:
+        data = args[0]
+
+        message = {'status': 'ok', 'predictions': []}
+
+        data_json = json.loads(data[0])
+        rows = np.array(data_json['rows'], dtype=np.float32)
+
+        global model
+        predictions = model.predict(rows)
+
+        i = 0
+        for prediction in predictions:
+            p = {
+                'prob': float(prediction[0])
+            }
+            if 'labels' in data_json:
+                p['label'] = data_json['labels'][i]
+            message.get('predictions').append(p)
+            i = i + 1
+    else:
+        message = 'Error reading input data'
     return message
 
 
