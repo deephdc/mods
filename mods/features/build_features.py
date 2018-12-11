912c264b716d3648557d34a7df16b6f315d3b6ac
stevo <stefan.dlugolinsky@savba.sk>
2018-12-11 14:47:31 +0100

    comments

--- mods/features/build_features.py
+++ mods/features/build_features.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-Feature building
+Created on Tue Jun 26 10:41:28 2018
+
+Building features from extracted datasets for ML/DL modeling,
+which runs in the similar way for Hadoop cluster and local machine
+but with different performance and scalability.
+
+@@author: stefan dlugolinsky
+@author: giangnguyen
+
+@@author: stefan dlugolinsky
+@author: giang nguyen
 """
 
 # import project config.py
