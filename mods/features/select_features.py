0edbd093badf6797b4f85197cd8500a54751b68b
giangzuzana <giang2005@gmail.com>
2019-08-06 15:35:05 +0200

    Intermittent Demand

--- mods/features/select_features.py
+++ mods/features/select_features.py
@@ -56,6 +56,117 @@ def test_linearity(filename):
     return
 
 
+# Intermittent Demand (ID)
+def test_ID_levels(filename):
+    print('\ntest intermittent demand levels:')
+
+    df = utl.create_df(filename)
+    print('nonzeros values: \n', df.astype(bool).sum(axis=0))
+
+    return
+
+
+# Intermittent Demand (ID)
+# https://medium.com/analytics-vidhya/croston-forecast-model-for-intermittent-demand-360287a17f5f
+def Croston(ts, extra_periods=1, alpha=0.4):
+    d = np.array(ts)
+    cols = len(d)
+
+    # Append np.nan into the demand array to cover future periods
+    d = np.append(d, [np.nan] * extra_periods)
+
+    # level (a), periodicity(p) and forecast (f)
+    a, p, f = np.full((3, cols + extra_periods), np.nan)
+    q = 1  # periods since last demand observation
+
+    # Initialization
+    first_occurence = np.argmax(d[:cols] > 0)
+    a[0] = d[first_occurence]
+    p[0] = 1 + first_occurence
+    f[0] = a[0] / p[0]
+    # Create all the t+1 forecasts
+    for t in range(0, cols):
+        if d[t] > 0:
+            a[t + 1] = alpha * d[t] + (1 - alpha) * a[t]
+            p[t + 1] = alpha * q + (1 - alpha) * p[t]
+            f[t + 1] = a[t + 1] / p[t + 1]
+            q = 1
+        else:
+            a[t + 1] = a[t]
+            p[t + 1] = p[t]
+            f[t + 1] = f[t]
+            q += 1
+
+    # Future Forecast
+    a[cols + 1:cols + extra_periods] = a[cols]
+    p[cols + 1:cols + extra_periods] = p[cols]
+    f[cols + 1:cols + extra_periods] = f[cols]
+
+    df = pd.DataFrame.from_dict({"Demand": d, "Forecast": f, "Period": p, "Level": a, "Error": d - f})
+    return df
+
+
+# Intermittent Demand (ID)
+# https://medium.com/analytics-vidhya/croston-forecast-model-for-intermittent-demand-360287a17f5f
+def Croston_TSB(ts, extra_periods=1, alpha=0.4, beta=0.4):
+    d = np.array(ts)
+    cols = len(d)
+
+    # Append np.nan into the demand array to cover future periods
+    d = np.append(d, [np.nan] * extra_periods)
+
+    # level (a), probability(p) and forecast (f)
+    a, p, f = np.full((3, cols + extra_periods), np.nan)
+    # Initialization
+    first_occurence = np.argmax(d[:cols] > 0)
+    a[0] = d[first_occurence]
+    p[0] = 1 / (1 + first_occurence)
+    f[0] = p[0] * a[0]
+
+    # Create all the t+1 forecasts
+    for t in range(0, cols):
+        if d[t] > 0:
+            a[t + 1] = alpha * d[t] + (1 - alpha) * a[t]
+            p[t + 1] = beta * (1) + (1 - beta) * p[t]
+        else:
+            a[t + 1] = a[t]
+            p[t + 1] = (1 - beta) * p[t]
+        f[t + 1] = p[t + 1] * a[t + 1]
+
+    # Future Forecast
+    a[cols + 1:cols + extra_periods] = a[cols]
+    p[cols + 1:cols + extra_periods] = p[cols]
+    f[cols + 1:cols + extra_periods] = f[cols]
+
+    df = pd.DataFrame.from_dict({"Demand": d, "Forecast": f, "Period": p, "Level": a, "Error": d - f})
+    return df
+
+
+
+# Autocorrelation (ACF) and partial autocorrelation (PACF)
+# https://stats.stackexchange.com/questions/81754/understanding-this-acf-output
+def test_autocorrelation(filename, seq_lags):
+    print('\ntest_autocorrelation:')
+
+    df = utl.create_df(filename)
+    for col in list(df):
+        print(col)
+        ts_data = df[col]
+        plt.plot(ts_data)
+        plt.show()
+
+        # autocorrelation
+        print(sm.graphics.tsa.acf(ts_data, nlags=seq_lags))
+        sm.graphics.tsa.plot_acf(ts_data, lags=seq_lags)
+        plt.show()
+
+        # partial autocorrelation
+        print(sm.graphics.tsa.acf(ts_data, nlags=seq_lags))
+        sm.graphics.tsa.plot_pacf(ts_data, lags=seq_lags)
+        plt.show()
+    return
+
+
 # generate random walk series for testing ADF
 def gen_random_walk(n=1000, plot=False):
     np.random.seed(1)
@@ -146,38 +257,17 @@ def test_mir(fn_in, fn_out='select_features_mir.tsv'):
             # print("f_score: \t", f_test)
     return
 
-# Autocorrelation and partial autocorrelation
-# https://stats.stackexchange.com/questions/81754/understanding-this-acf-output
-def test_autocorrelation(filename, seq_lags):
-    print('\ntest_autocorrelation:')
-
-    df = utl.create_df(filename)
-    for col in list(df):
-        print(col)
-        ts_data = df[col]
-        plt.plot(ts_data)
-        plt.show()
-
-        # autocorrelation
-        print(sm.graphics.tsa.acf(ts_data, nlags=seq_lags))
-        sm.graphics.tsa.plot_acf(ts_data, lags=seq_lags)
-        plt.show()
-
-        # partial autocorrelation
-        print(sm.graphics.tsa.acf(ts_data, nlags=seq_lags))
-        sm.graphics.tsa.plot_pacf(ts_data, lags=seq_lags)
-        plt.show()
-    return
-
 
 if __name__ == "__main__":
-#    print(cfg.BASE_DIR)
+    # print(cfg.BASE_DIR)
     
     # timestamp
     timer_start = datetime.datetime.now()
-
     fn = cfg.app_data + cfg.data_filename_train
+    # fn = cfg.app_data + cfg.data_filename_test
+
     # test_linearity(fn)
+    # test_ID_levels(fn)
     test_autocorrelation(fn, cfg.sequence_len)
     # test_adf(fn)
     # test_mir(fn)
