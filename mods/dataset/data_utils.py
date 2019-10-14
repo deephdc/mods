79f848ead47efc108f665c0b201fc85279d0b7ef
giangzuzana <giang2005@gmail.com>
2019-10-14 14:00:37 +0200

    apache 2.0

--- mods/dataset/data_utils.py
+++ mods/dataset/data_utils.py
@@ -1,9 +1,26 @@
 # -*- coding: utf-8 -*-
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""
+Created on Mon Oct 15 10:14:37 2018
+@author: stefan dlugolinsky
+"""
+
+
 import os
 import subprocess
-
-
-###
 
 
 def rclone_call(src_path, dest_dir, cmd='copy', get_output=False):
