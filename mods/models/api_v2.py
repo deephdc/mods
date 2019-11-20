9b373135c3c9b61367aa5ede17b415c05032c18b
Stevo Dlugolinsky <stevo@dlugolinsky.com>
2019-11-20 13:08:38 +0100

    TF 2.0.0

---
+++ mods/models/api_v2.py
@@ -0,0 +1,491 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""
+Created on Mon Nov 07 07:44:07 2019
+
+Train models with first order differential to monitor changes
+
+@author: stefan dlugolinsky
+@author: giang nguyen
+"""
+
+import os
+
+import pkg_resources
+from keras import backend
+from marshmallow import Schema, INCLUDE
+from webargs import fields
+
+import mods.config as cfg
+import mods.dataset.data_utils as dutils
+import mods.dataset.make_dataset as mdata
+import mods.models.mods_model as MODS
+import mods.utils as utl
+from mods.mods_types import TimeRange
+
+
+class TimeRangeField(fields.Field):
+    def _serialize(self, value: TimeRange, attr, obj, **kwargs):
+        return str(value)
+
+    def _deserialize(self, value: str, attr, data, **kwargs):
+        return TimeRange.from_str(value)
+
+
+# @dataclass
+# class TrainArgs:
+#     model_name: str
+#     data_select_query: str
+#     train_time_range: TimeRange
+#     train_time_ranges_excluded: list
+#     test_time_range: TimeRange
+#     test_time_ranges_excluded: list
+#     window_slide: str
+#     sequence_len: int
+#     model_delta: bool
+#     model_type: str
+#     num_epochs: int
+#     epochs_patience: int
+#     blocks: int
+#     steps_ahead: int
+#     batch_size: int
+#
+#
+# @dataclass
+# class PredictArgs:
+#     model_name: str
+#     data_select_query: str
+#     time_range: TimeRange
+#     time_ranges_excluded: list
+#     batch_size: int
+
+
+class TrainArgsSchema(Schema):
+    class Meta:
+        unknown = INCLUDE  # support 'full_paths' parameter
+
+    model_name = fields.Str(
+        required=False,
+        missing=cfg.model_name,
+        description="Name of the model. The model will be saved as a zip file; e.g., <model_name>.zip"
+    )
+    data_select_query = fields.Str(
+        required=False,
+        missing=cfg.train_data_select_query,
+        description= \
+            """
+            Query for data selection from the datapool.
+            
+            format: p1;p2|p2c1|col~p2c2_renamed|...;...#c5,c6,...
+                p1, p2, ...     - protocols; e.g., conn, http, ssh
+                p2c1, p2c2, ... - columns of the protocol
+                ~p2c2_renamed   - rename column
+                #c5, c6, ...    - columns to merge data over
+            """
+    )
+    train_time_range = TimeRangeField(
+        required=False,
+        missing=cfg.train_time_range,
+        description= \
+            """
+            Specify the time range for training; e.g., <2018-01-01,2019-01-01)
+            format: LBRACKET YYYY-MM-DD,YYYY-MM-DD RBRACKET
+                LBRACKET: left side bracket, closed: < or open: (
+                RBRACKET: right side bracket closed: > or open: )
+            """
+    )
+    train_time_ranges_excluded = fields.List(
+        TimeRangeField,
+        required=False,
+        missing=cfg.train_time_range_excluded,
+        description= \
+            """
+            A list of time ranges to skip. See the format for train or test time range.
+            If executing training from the command line, use ';' as time range delimiter.
+            """
+    )
+    test_time_range = TimeRangeField(
+        required=False,
+        missing=cfg.test_time_range,
+        description= \
+            """
+            Specify the time range for testing; e.g., <2019-01-01,2020-01-01)
+            format: LBRACKET YYYY-MM-DD,YYYY-MM-DD RBRACKET
+                LBRACKET: left side bracket, closed: < or open: (
+                RBRACKET: right side bracket closed: > or open: )
+            """
+    )
+    test_time_ranges_excluded = fields.List(
+        TimeRangeField,
+        required=False,
+        missing=cfg.test_time_range_excluded,
+        description= \
+            """
+            A list of time ranges to skip. See the format for train or test time range.
+            If executing training from the command line, use ';' as time range delimiter.
+            """
+    )
+    window_slide = fields.Str(
+        required=False,
+        missing=cfg.train_ws,
+        enum=cfg.train_ws_choices,
+        description="Window length and slide duration"
+    )
+    sequence_len = fields.Integer(
+        required=False,
+        missing=cfg.sequence_len,
+        description="length of the training sequence; e.g., 3, 4, 5, 6, 9, 12"
+    )
+    model_delta = fields.Boolean(
+        required=False,
+        missing=cfg.model_delta,
+        enum=[True, False],
+        description="Differential approach to model training"
+    )
+    model_type = fields.Str(
+        required=False,
+        missing=cfg.model_type,
+        enum=cfg.model_types,
+        description="Choose a type of the model"
+    )
+    num_epochs = fields.Integer(
+        required=False,
+        missing=cfg.num_epochs,
+        description="Number of training epochs"
+    )
+    epochs_patience = fields.Integer(
+        required=False,
+        missing=cfg.epochs_patience,
+        description=""
+    )
+    blocks = fields.Integer(
+        required=False,
+        missing=cfg.blocks,
+        description="Dimensionality of the model's output space"
+    )
+    steps_ahead = fields.Integer(
+        required=False,
+        missing=cfg.steps_ahead,
+        description="Number of steps ahead to predict"
+    )
+    batch_size = fields.Integer(
+        required=False,
+        missing=cfg.batch_size,
+        description="Training batch size"
+    )
+
+
+class PredictArgsSchema(Schema):
+    class Meta:
+        unknown = INCLUDE  # support 'full_paths' parameter
+
+    model_name = fields.Str(
+        required=False,
+        missing=cfg.model_name,
+        enum=cfg.model_name_all,
+        description="Choose model for prediction"
+    )
+    data_select_query = fields.Str(
+        required=False,
+        missing=cfg.train_data_select_query,
+        description= \
+            """
+            Query for data selection from the datapool.
+
+            format: p1;p2|p2c1|col~p2c2_renamed|...;...#c5,c6,...
+                p1, p2, ...     - protocols; e.g., conn, http, ssh
+                p2c1, p2c2, ... - columns of the protocol
+                ~p2c2_renamed   - rename column
+                #c5, c6, ...    - columns to merge data over
+            """
+    )
+    time_range = TimeRangeField(
+        required=False,
+        missing=cfg.test_time_range,
+        description= \
+            """
+            Specify the time range for training; e.g., <2018-01-01,2019-01-01)
+            format: LBRACKET YYYY-MM-DD,YYYY-MM-DD RBRACKET
+                LBRACKET: left side bracket, closed: < or open: (
+                RBRACKET: right side bracket closed: > or open: )
+            """
+    )
+    time_ranges_excluded = fields.List(
+        TimeRangeField,
+        required=False,
+        missing=cfg.test_time_range_excluded,
+        description= \
+            """
+            A list of time ranges to skip. See the format for prediction time range.
+            If executing prediction from the command line, use ';' as time range delimiter.
+            """
+    )
+    batch_size = fields.Integer(
+        required=False,
+        missing=cfg.batch_size,
+        description="Batch size"
+    )
+
+
+def load_model(
+        model_name=cfg.model_name,
+        models_dir=cfg.app_models
+):
+    """
+    Function loads existing MODS model
+
+    :param model_name: file name of the model ('zip' extension is optional)
+    :param models_dir:
+    :return: mods.models.mods_model
+    """
+    backend.clear_session()
+    m = MODS.mods_model(model_name)
+    m.load(os.path.join(models_dir, model_name))
+    return m
+
+
+def get_metadata():
+    """
+    https://docs.deep-hybrid-datacloud.eu/projects/deepaas/en/wip-api_v2/user/v2-api.html#deepaas.model.v2.base.BaseModel.get_metadata
+    :return:
+    """
+    module = __name__.split('.', 1)
+
+    pkg = pkg_resources.get_distribution(module[0])
+    meta = {
+        "author": "",
+        "author-email": "giang.nguyen@savba.sk, stefan.dlugolinsky@savba.sk",
+        "description": "",
+        "license": "",
+        "url": "https://github.com/deephdc/mods",
+        "version": "",
+    }
+
+    # override above values by values from PKG-INFO file (loads metadata from setup.cfg)
+    for l in pkg.get_metadata_lines("PKG-INFO"):
+        llo = l.lower()
+        for par in meta:
+            if llo.startswith(par.lower() + ":"):
+                _, v = l.split(": ", 1)
+                meta[par] = v
+
+    return meta
+
+
+def warm():
+    """
+    https://docs.deep-hybrid-datacloud.eu/projects/deepaas/en/wip-api_v2/user/v2-api.html#deepaas.model.v2.base.BaseModel.warm
+    :return:
+    """
+    # prepare the data
+    if not (os.path.exists(cfg.app_data_features) and os.path.isdir(cfg.app_data_features)):
+        mdata.prepare_data()
+
+
+def get_train_args(**kwargs):
+    """
+    https://docs.deep-hybrid-datacloud.eu/projects/deepaas/en/wip-api_v2/user/v2-api.html#deepaas.model.v2.base.BaseModel.get_train_args
+    https://marshmallow.readthedocs.io/en/latest/api_reference.html#module-marshmallow.fields
+    :param kwargs:
+    :return:
+    """
+    return TrainArgsSchema().fields
+
+
+def train(**kwargs):
+    """
+    https://docs.deep-hybrid-datacloud.eu/projects/deepaas/en/wip-api_v2/user/v2-api.html#deepaas.model.v2.base.BaseModel.train
+    """
+    print("train(**kwargs) - kwargs: %s" % (kwargs))
+
+    # use this schema
+    schema = TrainArgsSchema()
+    # deserialize key-word arguments
+    train_args = schema.load(kwargs)
+
+    print('train_args:', train_args)
+
+    model_name = train_args['model_name']
+
+    # support full paths for command line calls
+    models_dir = cfg.app_models
+    full_paths = train_args['full_paths'] if 'full_paths' in train_args else False
+    if full_paths:
+        print('full_paths:', full_paths)
+        models_dir = os.path.dirname(model_name)
+        model_name = os.path.basename(model_name)
+
+    train_time_range_excluded = train_args['train_time_ranges_excluded']
+    test_time_range_excluded = train_args['test_time_ranges_excluded']
+
+    # read train data from the datapool
+    df_train, cached_file_train = utl.datapool_read(
+        train_args['data_select_query'],
+        train_args['train_time_range'],
+        train_args['window_slide'],
+        train_time_range_excluded,
+        cfg.app_data_features
+    )
+    # repair the data
+    df_train = utl.fix_missing_num_values(df_train)
+
+    # read test data from the datapool
+    df_test, cached_file_test = utl.datapool_read(
+        train_args['data_select_query'],
+        train_args['train_time_range'],
+        train_args['window_slide'],
+        test_time_range_excluded,
+        cfg.app_data_features
+    )
+    # repair the data
+    df_test = utl.fix_missing_num_values(df_test)
+
+    backend.clear_session()
+    model = MODS.mods_model(model_name)
+    model.train(
+        df_train=df_train,
+        sequence_len=train_args['sequence_len'],
+        model_delta=train_args['model_delta'],
+        model_type=train_args['model_type'],
+        num_epochs=train_args['num_epochs'],
+        epochs_patience=train_args['epochs_patience'],
+        blocks=train_args['blocks'],
+        steps_ahead=train_args['steps_ahead'],
+        batch_size=train_args['batch_size']
+    )
+
+    # evaluate the model
+    predictions = model.predict(df_test)
+    metrics = utl.compute_metrics(
+        df_test[model.get_sequence_len():-train_args['steps_ahead']],
+        predictions[:-train_args['steps_ahead']],  # here, we predict # steps_ahead
+        model,
+    )
+
+    # put computed metrics into the model to be saved in model's zip
+    model.update_metrics(metrics)
+
+    # save model locally
+    file = model.save(os.path.join(models_dir, model_name))
+    dir_remote = cfg.app_models_remote
+
+    # upload model using rclone
+    if cfg.app_models_remote:
+        out, err = dutils.rclone_call(
+            src_path=file,
+            dest_dir=dir_remote,
+            cmd='copy'
+        )
+        print('rclone_copy(%s, %s):\nout: %s\nerr: %s' % (file, dir_remote, out, err))
+
+    message = {
+        'status': 'ok',
+        'dir_models': models_dir,
+        'model_name': model_name,
+        'steps_ahead': model.get_steps_ahead(),
+        'batch_size': model.get_batch_size(),
+        'window_slide': train_args['window_slide'],
+        'data_select_query': train_args['data_select_query'],
+        'train_time_range': str(train_args['train_time_range']),
+        'train_time_ranges_excluded': str(train_args['train_time_ranges_excluded']),
+        'train_cached_df': cached_file_train,
+        'test_time_range': str(train_args['test_time_range']),
+        'test_time_ranges_excluded': str(train_args['test_time_ranges_excluded']),
+        'test_cached_df': cached_file_test,
+        'evaluation': model.get_metrics(),
+    }
+
+    return message
+
+
+def get_predict_args():
+    """
+    https://docs.deep-hybrid-datacloud.eu/projects/deepaas/en/wip-api_v2/user/v2-api.html#deepaas.model.v2.base.BaseModel.get_predict_args
+    :return:
+    """
+    return PredictArgsSchema().fields
+
+
+def predict(**kwargs):
+    """
+    https://docs.deep-hybrid-datacloud.eu/projects/deepaas/en/wip-api_v2/user/v2-api.html#deepaas.model.v2.base.BaseModel.predict
+    :param kwargs:
+    :return:
+    """
+    print("predict(**kwargs) - kwargs: %s" % (kwargs))
+
+    # use this schema
+    schema = TrainArgsSchema()
+    # deserialize key-word arguments
+    predict_args = schema.load(kwargs)
+
+    print('predict_args:', predict_args)
+
+    model_name = predict_args['model_name']
+
+    # support full paths for command line calls
+    models_dir = cfg.app_models
+    full_paths = predict_args['full_paths'] if 'full_paths' in predict_args else False
+    if full_paths:
+        print('full_paths:', full_paths)
+        models_dir = os.path.dirname(model_name)
+        model_name = os.path.basename(model_name)
+
+    time_range_excluded = predict_args['time_ranges_excluded']
+
+    # read data from the datapool
+    df_data, cached_file_train = utl.datapool_read(
+        predict_args['data_select_query'],
+        predict_args['train_time_range'],
+        predict_args['window_slide'],
+        time_range_excluded,
+        cfg.app_data_features
+    )
+    # repair the data
+    df_data = utl.fix_missing_num_values(df_data)
+
+    backend.clear_session()
+    model = load_model(
+        models_dir=models_dir,
+        model_name=model_name
+    )
+
+    # override batch_size
+    batch_size = kwargs['batch_size']
+    model.set_batch_size(batch_size)
+
+    predictions = model.predict(df_data)
+
+    message = {
+        'status': 'ok',
+        'dir_models': models_dir,
+        'model_name': model_name,
+        'data_select_query': predict_args['data_select_query'],
+        'time_range': str(predict_args['time_range']),
+        'time_ranges_excluded': str(predict_args['time_ranges_excluded']),
+        'cached_df': cached_file_train,
+        'steps_ahead': model.get_steps_ahead(),
+        'batch_size': model.get_batch_size(),
+        'evaluation': utl.compute_metrics(
+            df_data[model.get_sequence_len():-model.get_steps_ahead()],
+            predictions[:-model.get_steps_ahead()],
+            model,
+        )
+    }
+
+    message['predictions'] = predictions.tolist()
+
+    return message
--- mods/utils.py
+++ mods/utils.py
@@ -28,6 +28,7 @@ from math import sqrt
 
 import numpy as np
 import pandas as pd
+import pytz
 from dateutil.relativedelta import *
 from numpy import dot
 from numpy.linalg import norm
@@ -35,6 +36,7 @@ from sklearn.metrics import mean_squared_error
 from sklearn.metrics import r2_score
 
 import mods.config as cfg
+from mods.mods_types import TimeRange
 
 
 # matplotlib.style.use('ggplot')
@@ -74,7 +76,7 @@ def create_df(filename):
     # in_sum_orig_bytes, in_sum_resp_bytes, out_sum_orig_bytes, out_sum_resp_bytes in MB or GB
     for feature in list(df):
         if '_bytes' in feature:
-            df[feature] = df[feature].div(1024*1024).astype(int)
+            df[feature] = df[feature].div(1024 * 1024).astype(int)
 
     print('create_df', filename, '\t', len(df.columns), df.shape, '\n', list(df))
     return df
@@ -147,7 +149,6 @@ def smape(y_true, y_pred):
         except ZeroDivisionError:
             score.append(str(np.nan))
     return score
-
 
 
 ##### @stevo @stevo @stevo#####
@@ -267,8 +268,9 @@ REGEX_SPLIT_COMMA = re.compile(r'\s*,\s*')
 REGEX_SPLIT_HASH = re.compile(r'\s*#\s*')
 REGEX_SPLIT_PIPE = re.compile(r'\s*\|\s*')
 REGEX_SPLIT_TILDE = re.compile(r'\s*~\s*')
-def parse_data_specs(specs):
 
+
+def parse_data_specs(specs):
     protocols = []
     merge_on_col = []
 
@@ -296,6 +298,8 @@ def parse_data_specs(specs):
 
 # @stevo
 REGEX_DATAPOOLTIME = re.compile(r'^\s*(?P<year>\d{4})([^0-9]{0,1}(?P<month>\d{2})([^0-9]{0,1}(?P<day>\d{2}))?)?\s*$')
+
+
 def parse_datetime(s):
     match = REGEX_DATAPOOLTIME.match(s)
     if match:
@@ -357,7 +361,10 @@ def expand_to_datetime_range(y, m, d, inclusive_end=cfg.time_range_inclusive):
 
 
 # @stevo
-REGEX_DATAPOOLTIMERANGE = re.compile(r'^\s*(?P<beg_year>\d{4})([^0-9]{0,1}(?P<beg_month>\d{2})([^0-9]{0,1}(?P<beg_day>\d{2}))?)?\s*--\s*(?P<end_year>\d{4})([^0-9]{0,1}(?P<end_month>\d{2})([^0-9]{0,1}(?P<end_day>\d{2}))?)?\s*$')
+REGEX_DATAPOOLTIMERANGE = re.compile(
+    r'^\s*(?P<beg_year>\d{4})([^0-9]{0,1}(?P<beg_month>\d{2})([^0-9]{0,1}(?P<beg_day>\d{2}))?)?\s*--\s*(?P<end_year>\d{4})([^0-9]{0,1}(?P<end_month>\d{2})([^0-9]{0,1}(?P<end_day>\d{2}))?)?\s*$')
+
+
 def parse_datetime_ranges(time_ranges):
     parsed = []
     if isinstance(time_ranges, str):
@@ -389,7 +396,7 @@ def parse_datetime_ranges(time_ranges):
                 m.group('end_year'),
                 m.group('end_month'),
                 m.group('end_day'),
-                is_end = True
+                is_end=True
             )
             parsed.append((beg, end))
             continue
@@ -397,11 +404,17 @@ def parse_datetime_ranges(time_ranges):
 
 
 # @stevo
-def is_within_range(d, range, inclusive_end=cfg.time_range_inclusive):
-    if inclusive_end:
-        return range[0] <= d and d <= range[1]
+def is_within_range(d, range: TimeRange):
+    if range.is_lclosed():
+        if range.is_rclosed():
+            return range.beg <= d and d <= range.end
+        else:
+            return range.beg <= d and d < range.end
     else:
-        return range[0] <= d and d < range[1]
+        if range.is_rclosed():
+            return range.beg < d and d <= range.end
+        else:
+            return range.beg < d and d < range.end
 
 
 # @stevo
@@ -413,11 +426,11 @@ def exclude(d, ranges):
 
 # @stevo datapool reading
 def datapool_read(
-        data_specs_str,                 # protocol/column/merge specification
-        time_range,                     # (beg datetime.datetime, end datetime.datetime)
-        ws,                             # window/slide specification; e.g., w01h-s10m
-        excluded=[],                    # list of dates and ranges that will be omitted
-        base_dir=cfg.app_data_features, # base dir with the protocol/YYYY/MM/DD/wXXd-sXXd.tsv structure
+        data_specs_str,  # protocol/column/merge specification
+        time_range,  # (beg datetime.datetime, end datetime.datetime)
+        ws,  # window/slide specification; e.g., w01h-s10m
+        excluded=[],  # list of dates and ranges that will be omitted
+        base_dir=cfg.app_data_features,  # base dir with the protocol/YYYY/MM/DD/wXXd-sXXd.tsv structure
         caching=cfg.data_pool_caching
 ):
     # regex matching directory of a day
@@ -485,7 +498,7 @@ def datapool_read(
                 day = int(rematch.group('day'))
 
                 # exclusion filter
-                dpt = datetime.datetime(year, month, day)
+                dpt = datetime.datetime(year, month, day, tzinfo=pytz.UTC)
 
                 data_file = os.path.join(root, f)
                 if exclude(dpt, excluded) or not is_within_range(dpt, time_range):
@@ -494,8 +507,9 @@ def datapool_read(
 
                 # load one of the data files
                 print('loading: %s' % data_file)
+                fp = open(data_file)
                 df = pd.read_csv(
-                    open(data_file),
+                    fp,
                     usecols=cols_orig,
                     header=0,
                     sep='\t',
@@ -503,6 +517,7 @@ def datapool_read(
                     skipfooter=0,
                     engine='python',
                 )
+                fp.close()
 
                 if cfg.fill_missing_rows_in_timeseries:
                     # fill missing rows for the loaded day
@@ -677,3 +692,5 @@ def fill_missing_rows(df, range_beg=None, range_end=None):
     if newnumrows > numrows:
         print('filled %d missing rows (was %d)' % (newnumrows - numrows, numrows))
     return df
+
+# @stevo
